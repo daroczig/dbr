ecbc9a2f97f76d79cfeb0b3c22dca1bf5eecf534
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2019-04-12 15:19:57 +0200

    optionally automatically apply a formatter on the sql statement

--- R/zzz.R
+++ R/zzz.R
@@ -8,6 +8,9 @@ dbs <- new.env(hash = TRUE, parent = emptyenv(), size = 29L)
     ## path to the default DB config YAML file
     options('dbr.db_config_path' = function() file.path(getwd(), 'db_config.yaml'))
 
+    ## default SQL formatter does not do anything
+    options('dbr.sql_formatter' = identity)
+
     ## by default, return data as standard data.frame
     ## as data.table or tibble might not be available / preferred
     options('dbr.output_format' = 'data.frame')
